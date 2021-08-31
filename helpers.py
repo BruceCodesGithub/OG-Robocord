fea86743c381ede9cacefb34b1ad59987fea9089
Bruce <86602706+BruceCodesGithub@users.noreply.github.com>
2021-08-31 10:27:39 +0530

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,266 @@
+import json
+import traceback
+import discord
+import sys
+import datetime
+import asyncpg
+import random
+import asyncio
+from discord.ext import commands
+
+
+class Bot(commands.Bot):
+
+    async def logout(self, *args, **kwargs):
+        if hasattr(self.pools, 'config'):
+            try:
+                await asyncio.wait_for(self.pools.config.close(), timeout=5)
+            except Exception as e:
+                print(e)
+        if hasattr(self, 'sr_api'):
+            try:
+                await asyncio.wait_for(self.sr_api.close(), timeout=5)
+            except Exception as e:
+                print(e)
+        if hasattr(self, 'wavelink'):
+            if not self.wavelink.session.closed:
+                await asyncio.wait_for(self.wavelink.session.close(),
+                                       timeout=5)
+        await super().logout(*args, **kwargs)
+
+
+class Embed(discord.Embed):
+    def __repr__(self):
+        return self.description or ''
+
+
+class Context(commands.Context):
+
+    Embed = Embed
+
+    def embed(self, description=None, *args, **kwargs):
+        discord_colors = {
+            'Blurple': 0x5865F2,
+            'Green': 0x57F287,
+            'Yellow': 0xFEE75C,
+            'Fuchsia': 0xEB459E,
+            'Red': 0xed4245,
+            'White': 0xFFFFFE,
+            'Black': 0x23272A
+            }
+        default = {
+            'timestamp': self.message.created_at,
+            'description': description,
+            'color': random.choice([discord_colors[color] for color in discord_colors])
+            }
+        default.update(kwargs)
+        return_embed = self.Embed(*args, **default)
+        return_embed.set_footer(icon_url=self.author.avatar_url,
+                                text=f'Requested by {self.author}')
+        return return_embed
+
+    def error(self, description=None, *args, **kwargs):
+        default = {
+            'title': 'Error',
+            'color': discord.Color.red(),
+            'timestamp': self.message.created_at,
+            'description': description
+        }
+        default.update(kwargs)
+        return_embed = self.Embed(*args, **default)
+        return_embed.set_author(name=self.author,
+                                icon_url=self.author.avatar_url)
+        return_embed.set_footer(icon_url=self.bot.user.avatar_url, text=(
+            'If you think this is a mistake please contact '
+            f'{self.bot.get_user(self.bot.owner_ids[0])}'))
+        return return_embed
+
+    def success(self, description=None, *args, **kwargs):
+        default = {
+            'title': 'Success',
+            'color': discord.Color.green(),
+            'timestamp': self.message.created_at,
+            'description': description
+            }
+        default.update(kwargs)
+        return_embed = self.Embed(*args, **default)
+        return_embed.set_author(name=self.author,
+                                icon_url=self.author.avatar_url)
+        return_embed.set_footer(icon_url=self.bot.user.avatar_url, text='Action successful')
+        return return_embed
+
+    async def send_embed(self, *args, **kwargs):
+        await self.send(embed=self.embed(*args, **kwargs))
+
+    async def send_error(self, *args, **kwargs):
+        await self.send(embed=self.error(*args, **kwargs))
+
+    async def send_success(self, *args, **kwargs):
+        await self.send(embed=self.success(*args, **kwargs))
+
+
+async def init_connection(connection):
+    await connection.set_type_codec(
+        'json',
+        encoder=json.dumps,
+        decoder=json.loads,
+        schema='pg_catalog'
+    )
+
+
+def storage(bot, key=None, value=None, method=None, override=False):
+    try:
+        with open('./storage/config.json', 'r') as f:
+            data = json.load(f)
+    except OSError:
+        with open('./storage/config.json', 'w+') as f:
+            f.write('{}')
+        with open('./storage/config.json', 'r') as f:
+            data = json.load(f)
+    data['cogs'] = data.get('cogs', [])
+    data['blacklisted'] = data.get('blacklisted', [])
+    data['disabled'] = data.get('disabled', False)
+    if bot.default_owner:
+        temp_owner = int(bot.default_owner)
+    else:
+        temp_owner = 571638000661037056
+    data['owners'] = data.get('owners', [temp_owner])
+    bot.restart_channel = data.get('restart_channel', None)
+    data['restart_channel'] = None
+    if key and value is not None:
+        if method == 'append':
+            if value not in data[key] or override:
+                data[key].append(value)
+        elif method == 'remove':
+            if value in data[key] or override:
+                data[key].remove(value)
+        else:
+            data[key] = value
+    with open('./storage/config.json', 'w') as f:
+        json.dump(data, f, indent=4)
+    return data
+
+
+async def prepare(bot, guild=None):
+    try:
+        connection = await bot.pools.config.acquire()
+        await bot.pools.config.release(connection)
+    except asyncpg.exceptions._base.InterfaceError:
+        try:
+            bot.pools.config = await asyncio.wait_for(asyncpg.create_pool(
+                database='codingbot_db', init=init_connection), timeout=5)
+            connection = await bot.pools.config.acquire()
+            await bot.pools.config.release(connection)
+        except (OSError, asyncpg.exceptions._base.InterfaceError,
+                asyncio.exceptions.TimeoutError):
+            if guild:
+                bot.server_cache[guild.id] = bot.server_cache.get(guild.id, {
+                    'prefixes': bot.default_prefixes.copy(),
+                    'commands': {}
+                })
+            return
+    async with bot.pools.config.acquire() as connection:
+        if guild:
+            await connection.execute('''
+                CREATE TABLE IF NOT EXISTS serverconf (
+                    id bigint,
+                    commands json,
+                    prefixes text[]
+                );
+            ''')
+            data = await connection.fetchrow(
+                'SELECT * FROM serverconf WHERE id = $1', guild.id)
+            bot.server_cache[guild.id] = bot.server_cache.get(guild.id, {
+                'prefixes': bot.default_prefixes.copy(),
+                'commands': {}
+            })
+            if data:
+                if isinstance(data['prefixes'], list):
+                    bot.server_cache[guild.id]['prefixes'] = data['prefixes']
+                if isinstance(data['commands'], dict):
+                    bot.server_cache[guild.id]['commands'] = data['commands']
+
+
+async def is_disabled(ctx):
+    if not ctx.guild:
+        return False
+    try:
+        data = ctx.bot.server_cache[ctx.guild.id].copy()
+    except KeyError:
+        await prepare(ctx.bot, ctx.guild)
+        try:
+            data = ctx.bot.server_cache[ctx.guild.id].copy()
+        except KeyError:
+            data = {}
+    ids_to_check = [ctx.guild.id, ctx.channel.id, ctx.author.id]
+    ids_to_check += [r.id for r in ctx.author.roles]
+    for id_ in ids_to_check:
+        data[int(id_)] = data.get(int(id_), [])
+        if (True in data[int(id_)]
+                or ctx.command.qualified_name in data[int(id_)]):
+            return True
+    return False
+
+
+async def prefix(bot, message):
+    return_prefixes = bot.default_prefixes.copy()
+    if not message.guild:
+        return_prefixes.append('')
+    else:
+        try:
+            data = bot.server_cache[message.guild.id]['prefixes']
+        except KeyError:
+            try:
+                bot.server_cache[message.guild.id] = bot.server_cache.get(
+                    message.guild.id, {
+                        'prefixes': return_prefixes,
+                        'commands': {}
+                    })
+                bot.loop.create_task(prepare(bot, message.guild))
+                data = bot.server_cache[message.guild.id]['prefixes']
+            except KeyError:
+                data = bot.default_prefixes
+        return_prefixes = data or return_prefixes
+    return return_prefixes
+
+
+async def log_command_error(ctx, error, handled):
+    if not handled:
+        channel = ctx.bot.get_channel(875972719143956500)
+    else:
+        channel = ctx.bot.get_channel(875973025424621598)
+    title = 'Ignoring exception in command {}:'.format(ctx.command)
+    err = ''.join(traceback.format_exception(
+        type(error), error, error.__traceback__))
+    try:
+        embed = discord.Embed(title=title, description=f'```py\n{err}```',
+                              timestamp=ctx.message.created_at,
+                              color=discord.Color.red())
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        await channel.send(embed=embed)
+    except discord.errors.Forbidden:
+        try:
+            await channel.send((f"**<@{ctx.bot.owner_ids[0]}> An error "
+                                "occurred but I couldn't log it here**"))
+        except discord.errors.Forbidden:
+            pass
+        print('Ignoring exception in command {}:'.format(ctx.command),
+              file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__,
+                                  file=sys.stderr)
+    finally:
+        return
+
+
+async def log_error(bot, event_method, *args, **kwargs):
+    channel = bot.get_channel(875972719143956500)
+    try:
+        title = 'Ignoring exception in {}'.format(event_method)
+        err = ''.join(traceback.format_exc())
+        embed = discord.Embed(title=title, description=f'```py\n{err}```',
+                              timestamp=datetime.datetime.utcnow(),
+                              color=discord.Color.red())
+        await channel.send(embed=embed)
+    except discord.errors.Forbidden:
+        print('Ignoring exception in {}'.format(event_method), file=sys.stderr)
+        traceback.print_exc()
---
+++ main.py
@@ -0,0 +1,322 @@
+# from keep_alive import keep_alive
+import discord
+from discord.ext import commands, tasks
+from discord.ext.commands import when_mentioned_or, command, has_permissions, MissingPermissions, cooldown, BucketType
+import datetime
+import asyncio, aiohttp
+from discord import DMChannel
+import requests, humanize, json, asyncpg
+import calendar, math, unicodedata, random, os, time, io
+import ext.helpers as helpers
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from dotenv import load_dotenv
+
+global embed_style1
+embed_style1 = discord.Embed()
+
+
+
+class HelpCommand(commands.HelpCommand):
+  
+    def get_ending_note(self):
+        return 'Use {0}{1} [command] for more info on a command.'.format(
+            self.clean_prefix, self.invoked_with)
+
+    def get_command_signature(self, command):
+        parent = command.full_parent_name
+        if len(command.aliases) > 0:
+            aliases = '|'.join(command.aliases)
+            fmt = f'[{command.name}|{aliases}]'
+            if parent:
+                fmt = f'{parent}, {fmt}'
+            alias = fmt
+        else:
+            alias = command.name if not parent else f'{parent} {command.name}'
+        return f'{alias} {command.signature}'
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title='Pycord-Chan', color=discord.Color.blurple())
+        description = self.context.bot.description
+        if description:
+            embed.description = description
+
+        for cog_, cmds in mapping.items():
+            name = 'Other Commands' if cog_ is None else cog_.qualified_name
+            filtered = await self.filter_commands(cmds, sort=True)
+            if filtered:
+                value = '\u2002'.join(c.name for c in cmds)
+                if cog_ and cog_.description:
+                    value = '{0}\n{1}'.format(cog_.description, value)
+
+                embed.add_field(name=name, value=value, inline=False)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+
+    async def send_cog_help(self, cog_):
+        embed = discord.Embed(title='{0.qualified_name} Commands'.format(cog_))
+        if cog_.description:
+            embed.description = cog_.description
+
+        filtered = await self.filter_commands(cog_.get_commands(), sort=True)
+        for command in filtered:
+            embed.add_field(name=self.get_command_signature(command),
+                            value=command.short_doc or '...', inline=False)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+
+    async def send_group_help(self, group):
+        embed = discord.Embed(title=group.qualified_name)
+        if group.help:
+            embed.description = group.help
+
+        if isinstance(group, commands.Group):
+            filtered = await self.filter_commands(group.commands, sort=True)
+            for command in filtered:
+                embed.add_field(name=self.get_command_signature(command),
+                                value=command.short_doc or '...', inline=False)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+
+    # This makes it so it uses the function above
+    # Less work for us to do since they're both similar.
+    # If you want to make regular command help look different then override it
+    send_command_help = send_group_help
+
+
+bot = commands.Bot(command_prefix="p!", description='The bot build with and for pycord.',
+                  case_insensitive=True, embed_color=discord.Color.blurple(),
+                  help_command=HelpCommand(), activity=discord.Activity(type=discord.ActivityType.competing, name="What's dpy's Best Fork?"), status=discord.Status.online)
+
+bot.load_extension('jishaku')
+bot.load_extension('modules.fun')
+bot.default_owner = 571638000661037056
+
+class pools:
+    config = asyncpg.create_pool(database='codingbot_db',
+                                 init=helpers.init_connection)
+
+@bot.event
+async def on_invite_create(invite):
+    await bot.tracker.update_invite_cache(invite)
+
+
+@bot.event
+async def on_invite_delete(invite):
+    await bot.tracker.remove_invite_cache(invite)
+
+os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+os.environ['JISHAKU_RETAIN'] = "True"
+
+
+
+init_data = helpers.storage(bot)
+
+bot.helpers = helpers
+bot.default_prefixes = ['p!']
+bot.server_cache = {}
+bot.pools = pools
+bot.owner_id = 571638000661037056
+bot.owner_ids = init_data['owners']
+bot.blacklisted = init_data['blacklisted']
+bot.disabled = init_data['disabled']
+bot.active_cogs = init_data['cogs']
+bot.server_cache = {}
+async def prefix(bot_, message):
+    return commands.when_mentioned_or(*(await helpers.prefix(bot_, message)))(
+        bot_, message)
+@bot.event
+async def on_ready():
+    print("{} is Ready and Online!".format(bot.user))
+
+@bot.event
+async def on_message(message):
+  if message.author.bot == True:
+    return
+  if message.channel.id == 750218011230601241:
+    role = discord.utils.find(lambda r: r.id == 721216973035274300, message.guild.roles)
+    if role in message.author.roles:
+      return
+    else:
+      embed=discord.Embed(title="Suggestion", description=f"{message.content}", color=discord.Color.blurple())
+      embed.set_footer(text=f"{message.author.name}", icon_url=f"{message.author.avatar_url}")
+      message1 = await message.channel.send(embed=embed)
+      await message1.add_reaction("✅")
+      await message1.add_reaction("🤷")
+      await message1.add_reaction("❌")
+    
+      await message.delete()
+  await bot.process_commands(message)
+
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    exception = error
+    if hasattr(ctx.command, 'on_error'):
+        pass
+    error = getattr(error, 'original', error)
+
+
+    if ctx.author.id in ctx.bot.owner_ids:
+        if (isinstance(error, (
+                commands.MissingAnyRole,
+                commands.CheckFailure,
+                commands.DisabledCommand, commands.CommandOnCooldown,
+                commands.MissingPermissions, commands.MaxConcurrencyReached))):
+            try:
+              await ctx.reinvoke()
+            except discord.ext.commands.CommandError as e:
+              pass
+            else:
+                return
+
+
+    if (isinstance(error, (
+            commands.BadArgument, commands.MissingRequiredArgument,
+            commands.NoPrivateMessage, commands.CheckFailure,
+            commands.DisabledCommand, commands.CommandInvokeError,
+            commands.TooManyArguments, commands.UserInputError,
+            commands.NotOwner, commands.MissingPermissions,
+            commands.BotMissingPermissions, commands.MaxConcurrencyReached,
+            commands.CommandNotFound))):
+        await helpers.log_command_error(ctx, exception, True)
+        if not isinstance(error, commands.CommandNotFound):
+            if await helpers.is_disabled(ctx):
+                return  
+        text = None
+        if isinstance(error, commands.CheckFailure):
+            if bot.disabled:
+                text = 'The bot is currently disabled. It will be back soon.'
+        if not isinstance(error, commands.CommandNotFound):
+            embed = discord.Embed(title="Error", description=text or str(error),
+                              color=discord.Color.red())
+            embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+            owner = bot.get_user(ctx.bot.owner_ids[0])
+            embed.set_footer(
+                icon_url=bot.user.avatar_url,
+                text=f'If you think this is a mistake please contact {owner}')
+            await ctx.send(embed=embed)
+
+    elif isinstance(error, commands.CommandOnCooldown):
+        await helpers.log_command_error(ctx, exception, True)
+        time2 = datetime.timedelta(seconds=math.ceil(error.retry_after))
+        error = (f'You are on cooldown. Try again after {humanize.precisedelta(time2)}')
+        embed = discord.Embed(title="Error", description=error,
+                          color=discord.Color.red())
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        owner = bot.get_user(ctx.bot.owner_ids[0])
+        embed.set_footer(
+            icon_url=bot.user.avatar_url,
+            text=f'If you think this is a mistake please contact {owner}')
+        await ctx.send(embed=embed)
+
+    else:
+        try:
+            raise error
+            embed = discord.Embed(title='Oh no!', description=(
+                'An error occurred. My developer has been notified of it, '
+                'but if it continues to occur please DM '
+                f'<@{ctx.bot.owner_ids[0]}>'), color=discord.Color.red())
+            await ctx.send(embed=embed)
+        except discord.errors.Forbidden:
+            pass
+        await helpers.log_command_error(ctx, exception, False)
+
+# @bot.event
+# async def on_command_error(ctx, error):
+#   if isinstance(error, commands.CommandOnCooldown):
+#     await ctx.send(embed=discord.Embed(description=f'Command is still in cooldown. Try again after {humanize.precisedelta(time)}'))
+
+#   elif isinstance(error, commands.Notowner):
+#     await ctx.send(embed=discord.Embed(description=f'This command is only for the owner of the bot, and can only be used by him.'))
+
+#   elif isinstance(error, commands.MissingAnyRole):
+#     await ctx.send(embed=discord.Embed(description=f'You are missing the required roles to run this command. - '))
+
+bot.launch_time = datetime.datetime.utcnow()
+
+
+class Misc(commands.Cog):
+    @commands.command(pass_context=True)
+    async def cats(self, ctx):
+     embed = discord.Embed(title="Cute Cats Pictures :)", description="")
+     embed.set_footer(text="From the subreddit r/cats", icon_url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/274/cat-face_1f431.png")
+
+     async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/cats/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed) 
+    @commands.command()
+    async def dogs(self, ctx):
+     embed = discord.Embed(title="Cute Dog Pictures :)", description="")
+     embed.set_footer(text="From the subreddit r/DogPics", icon_url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/274/dog_1f415.png")
+
+     async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/DogPics/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed)
+
+    @commands.command()
+    async def goats(self, ctx):
+     embed = discord.Embed(title="Cute Goat Pictures :)", description="Twiggy wanted this")
+     embed.set_footer(text="From the subreddit r/goatpics")
+
+     async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/goatpics/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed) 
+
+    @commands.command()
+    async def cute(self, ctx):
+     embed = discord.Embed(title="Cute Pictures ᵔᴥᵔ", description="Awww ᵔᴥᵔ")
+     embed.set_footer(text="From the subreddit r/cute")
+
+     async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/cute/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed)
+
+
+bot.add_cog(Misc(bot))
+
+
+# @bot.command()
+# async def timenow(ctx):
+  
+#   d = datetime.utcnow()
+#   unixtime = calendar.timegm(d.utctimetuple())
+#   await ctx.send(f"<t:{unixtime}:t>")
+
+
+@bot.command()
+async def ping(ctx):
+        loading = '<:thinkCat:853565931838242816>'
+        ws_ping = f'{(bot.latency * 1000):.2f}ms ' \
+                  f'({humanize.precisedelta(datetime.timedelta(seconds=bot.latency))})'
+        embed = discord.Embed(title='PONG!  :ping_pong:', description=(
+            f'**{loading} Websocket:** {ws_ping}\n** :repeat: Round-Trip:** Calculating...'), color=discord.Color.blurple())
+        start = time.perf_counter()
+        message = await ctx.send(embed=embed)
+        end = time.perf_counter()
+        await asyncio.sleep(0.5)
+        trip = end - start
+        rt_ping = f'{(trip * 1000):.2f}ms ({humanize.precisedelta(datetime.timedelta(seconds=trip))})'
+        embed.description = (
+            f'**{loading} Websocket:** {ws_ping}\n**'
+            f':repeat: Round-Trip:** {rt_ping}.')
+        await message.edit(embed=embed)
+        await asyncio.sleep(0.5)
+        start = time.perf_counter()
+        await message.edit(embed=embed)
+
+
+
+# keep_alive()
+bot.run(os.getenv("TOKEN"))
