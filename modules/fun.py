5aa81b2de90043f8480ce994680d474f28856d7b
MarzaElise <anfasalma2007@gmail.com>
2021-08-31 21:06:21 +0530

    Formatted with black

--- modules/fun.py
+++ modules/fun.py
@@ -7,37 +7,64 @@ import animec
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
     @commands.command(name="anime")
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def _anime(self, ctx, *, query):
-      try:
-        anime = animec.Anime(query)
-      except:
-        await ctx.send(embed = discord.Embed(description="No anime with that name was found."))
-        return
-      
-      embed = discord.Embed(title=anime.title_english, url = anime.url, description=f"{anime.description[:200]}...", color=discord.Color.random())
-      embed.add_field(name="Rating", value=f"{anime.rating}", inline=True)
-      embed.add_field(name="Ranking", value=f"{anime.ranked}", inline=True)
-      embed.add_field(name="Status", value=f"{anime.status}", inline=False)
-      embed.add_field(name="Episodes", value=f"{anime.episodes}", inline=False)
-      embed.set_image(url=anime.poster)
-      if anime.rating == "Rx - Hentai":
-        await ctx.send("Anime not found.")
-      else:
-        
-        await ctx.send(embed=embed)
+        try:
+            anime = animec.Anime(query)
+        except:
+            await ctx.send(
+                embed=discord.Embed(description="No anime with that name was found.")
+            )
+            return
+
+        embed = discord.Embed(
+            title=anime.title_english,
+            url=anime.url,
+            description=f"{anime.description[:200]}...",
+            color=discord.Color.random(),
+        )
+        embed.add_field(name="Rating", value=f"{anime.rating}", inline=True)
+        embed.add_field(name="Ranking", value=f"{anime.ranked}", inline=True)
+        embed.add_field(name="Status", value=f"{anime.status}", inline=False)
+        embed.add_field(name="Episodes", value=f"{anime.episodes}", inline=False)
+        embed.set_image(url=anime.poster)
+        if anime.rating == "Rx - Hentai":
+            await ctx.send("Anime not found.")
+        else:
+
+            await ctx.send(embed=embed)
 
     @commands.command(name="aninews")
     async def _anime_news(self, ctx):
-      news = animec.aninews.Aninews(amount=5)
-      embed = discord.Embed(title="Anime news", description="For the weeb army and in the honor of Atomic Baby", color=discord.Color.random())
-      embed.add_field(name=news.titles[0], value=f"{news.description[0][:400]}... [Continue Reading]({news.links[0]})")
-      embed.add_field(name=news.titles[1], value=f"{news.description[1][:300]}... [Continue Reading]({news.links[1]})")
-      embed.add_field(name=news.titles[2], value=f"{news.description[2][:300]}... [Continue Reading]({news.links[2]})")
-      embed.add_field(name=news.titles[3], value=f"{news.description[3][:200]}... [Continue Reading]({news.links[3]})")
-      embed.add_field(name=news.titles[4], value=f"{news.description[4][:200]}... [Continue Reading]({news.links[4]})")
-      await ctx.send(embed = embed)
+        news = animec.aninews.Aninews(amount=5)
+        embed = discord.Embed(
+            title="Anime news",
+            description="For the weeb army and in the honor of Atomic Baby",
+            color=discord.Color.random(),
+        )
+        embed.add_field(
+            name=news.titles[0],
+            value=f"{news.description[0][:400]}... [Continue Reading]({news.links[0]})",
+        )
+        embed.add_field(
+            name=news.titles[1],
+            value=f"{news.description[1][:300]}... [Continue Reading]({news.links[1]})",
+        )
+        embed.add_field(
+            name=news.titles[2],
+            value=f"{news.description[2][:300]}... [Continue Reading]({news.links[2]})",
+        )
+        embed.add_field(
+            name=news.titles[3],
+            value=f"{news.description[3][:200]}... [Continue Reading]({news.links[3]})",
+        )
+        embed.add_field(
+            name=news.titles[4],
+            value=f"{news.description[4][:200]}... [Continue Reading]({news.links[4]})",
+        )
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
--- modules/tags.py
+++ modules/tags.py
@@ -2,18 +2,20 @@ import discord
 from discord.ext import commands
 from cogs.database import Database
 
+
 class Tags(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = Database(self.bot)
 
     @commands.group(invoke_without_command)
-    async def tag(self,name):
+    async def tag(self, name):
         await self.db.show(name)
 
     @tag.command()
-    async def create(self,name,value):
-        await self.db.new(name,value)
+    async def create(self, name, value):
+        await self.db.new(name, value)
+
 
 def setup(bot):
     bot.add_cog(Tags(bot))
